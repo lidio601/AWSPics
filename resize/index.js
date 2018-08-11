5917966d58c91694586119a56c8f0cdf730c65aa
Fabio Cigliano <fabio.cigliano@gmail.com>
2018-08-11 17:20:37 +0200

    resize lambda fixes

--- resize/index.js
+++ resize/index.js
@@ -15,10 +15,12 @@ const im = gm_1.default.subClass({
 });
 const s3 = new aws_sdk_1.default.S3();
 const cloudfront = new aws_sdk_1.default.CloudFront();
+const sizes = ["1200x750", "360x225"];
 const getImageType = (objectContentType) => {
     const res = mime_1.default.getExtension(objectContentType);
-    if (!lodash_1.default.startsWith(res || "", "image")) {
-        throw new Error("unsupported objectContentType " + objectContentType);
+    if (!lodash_1.default.startsWith(objectContentType, "image")) {
+        console.log("unsupported objectContentType " + objectContentType);
+        return "";
     }
     return res || "";
 };
@@ -31,7 +33,7 @@ const cross = (left, right) => {
     });
     return res;
 };
-function invalidateCloudFront(imagePaths, cb) {
+function invalidateCloudFront(cb) {
     cloudfront.listDistributions((err, data) => {
         // Handle error
         if (err) {
@@ -43,50 +45,83 @@ function invalidateCloudFront(imagePaths, cb) {
             return d.DomainName === distributionDomain;
         }).Id;
         // Create invalidation
+        console.log("Cloudfront invalidating /*");
         cloudfront.createInvalidation({
             DistributionId: distributionID,
             InvalidationBatch: {
                 CallerReference: "resize-" + Date.now(),
                 Paths: {
-                    Items: imagePaths || ["/*"],
+                    Items: ["/*"],
                     Quantity: 1,
                 },
             },
-        }, (err, data) => {
+        }, (err) => {
             if (err)
                 console.log(err, err.stack);
             cb(err);
         });
     });
 }
-function handler(event, context) {
-    console.log("event ", JSON.stringify(event));
-    async_1.default.mapLimit(event.Records, 4, (record, cb) => {
+/*
+ * Sample event put
+{
+  "Records": [
+    {
+      "s3": {
+        "object": {
+          "key": "HappyFace.jpg",
+          ...
+        },
+        "bucket": {
+          "arn": bucketarn,
+          "name": "sourcebucket",
+          ...
+        },
+      },
+      ...
+      "eventName": "ObjectCreated:Put",
+      "eventSource": "aws:s3"
+    }
+  ]
+}
+*/
+function handlePutEvent(records, cb) {
+    // retrieve all the matching images
+    async_1.default.mapLimit(records, 4, (record, cb) => {
         const originalKey = decodeURIComponent(record.s3.object.key.replace(/\+/g, " "));
         s3.getObject({
             Bucket: record.s3.bucket.name,
             Key: originalKey,
         }, (err, data) => {
             if (err) {
-                cb(err);
+                cb(null, {} /*err*/);
             }
             else {
-                cb(null, {
-                    buffer: data.Body,
-                    contentType: data.ContentType,
-                    imageType: getImageType(data.ContentType),
-                    originalKey,
-                    record,
-                });
+                const imageType = getImageType(data.ContentType);
+                if (lodash_1.default.isEmpty(imageType))
+                    cb(null, {});
+                else
+                    cb(null, {
+                        buffer: data.Body,
+                        contentType: data.ContentType,
+                        imageType,
+                        originalKey,
+                        record,
+                    });
             }
         });
     }, (err, images) => {
+        if (images)
+            images = lodash_1.default.filter(images);
         if (err) {
-            context.fail(err);
+            cb(err);
+        }
+        else if (!lodash_1.default.size(images)) {
+            cb();
         }
         else {
-            const imagePaths = [];
-            const resizePairs = cross(["1200x750", "360x225"], images);
+            // produce image thumbnails
+            const resizePairs = cross(sizes, images);
             async_1.default.eachLimit(resizePairs, 4, (resizePair, cb) => {
                 const config = resizePair[0];
                 const image = resizePair[1];
@@ -94,33 +129,87 @@ function handler(event, context) {
                 const width = config.split("x")[0];
                 const height = config.split("x")[1];
                 let operation = im(image.buffer).resize(width, height, "^");
-                imagePaths.push(image.originalKey);
                 if (config === "360x225") {
                     operation = operation.gravity("Center").crop(width, height);
                 }
                 operation.toBuffer(image.imageType, (err3, buffer) => {
-                    if (err3) {
+                    if (err3)
                         cb(err3);
-                    }
                     else {
+                        const resizePath = "pics/resized/" + config + "/" + relativePath;
+                        console.log("putting resize image", resizePath);
                         s3.putObject({
                             Body: buffer,
                             Bucket: bucketName,
                             ContentType: image.contentType,
-                            Key: "pics/resized/" + config + "/" + relativePath,
-                        }, (err2) => {
-                            cb(err2);
-                            imagePaths.push("pics/resized/" + config + "/" + relativePath);
-                        });
+                            Key: resizePath,
+                        }, cb);
                     }
                 });
-            }, (err1) => {
-                if (err1)
-                    context.fail(err1);
+            }, cb);
+        }
+    });
+}
+/*
+ * Sample event delete
+{
+  "Records": [
+    {
+      ...
+      "s3": {
+        "object": {
+          "key": "HappyFace.jpg"
+          ...
+        },
+        "bucket": {
+          "arn": bucketarn,
+          "name": "sourcebucket"
+          ...
+        }
+      },
+      ...
+      "eventName": "ObjectRemoved:Delete",
+      "eventSource": "aws:s3"
+    }
+  ]
+}
+ */
+function handleDeleteEvent(records, cb) {
+    // retrieve all the matching images
+    async_1.default.mapLimit(records, 4, (record, cb) => {
+        const originalKey = decodeURIComponent(record.s3.object.key.replace(/\+/g, " "));
+        const relativePath = originalKey.replace("pics/original/", "");
+        const resizePairs = cross(sizes, [originalKey]);
+        async_1.default.eachLimit(resizePairs, 4, (resizePair, cb) => {
+            const config = resizePair[0];
+            const image = resizePair[1];
+            const resizePath = "pics/resized/" + config + "/" + relativePath;
+            console.log("deleting resized image", resizePath);
+            s3.deleteObject({
+                Bucket: bucketName,
+                Key: resizePath,
+            }, cb);
+        });
+    }, cb);
+}
+function handler(event, context) {
+    console.log("event ", JSON.stringify(event));
+    const records = event.Records || [];
+    const putRecords = lodash_1.default.filter(records, r => lodash_1.default.isEqual(lodash_1.default.get(r, "eventName"), "ObjectCreated:Put"));
+    const deleteRecords = lodash_1.default.filter(records, r => lodash_1.default.isEqual(lodash_1.default.get(r, "eventName"), "ObjectRemoved:Delete"));
+    console.log("got", lodash_1.default.size(putRecords), "records on put");
+    console.log("got", lodash_1.default.size(deleteRecords), "records on delete");
+    handlePutEvent(putRecords, (err) => {
+        if (err)
+            context.fail(err);
+        else {
+            handleDeleteEvent(deleteRecords, (err) => {
+                if (err)
+                    context.fail(err);
                 else
-                    invalidateCloudFront(imagePaths, (err2) => {
-                        if (err2)
-                            context.fail(err2);
+                    invalidateCloudFront((err3) => {
+                        if (err3)
+                            context.fail(err);
                         else
                             context.succeed();
                     });
--- resize/index.ts
+++ resize/index.ts
@@ -11,11 +11,13 @@ const im = gm.subClass({
 });
 const s3 = new AWS.S3();
 const cloudfront = new AWS.CloudFront();
+const sizes = ["1200x750", "360x225"];
 
 const getImageType = (objectContentType): string => {
     const res: string | null = mime.getExtension(objectContentType);
-    if (!_.startsWith(res || "", "image")) {
-        throw new Error("unsupported objectContentType " + objectContentType);
+    if (!_.startsWith(objectContentType, "image")) {
+        console.log("unsupported objectContentType " + objectContentType);
+        return "";
     }
 
     return res || "";
@@ -33,7 +35,7 @@ const cross = (left, right): any[] => {
     return res;
 };
 
-function invalidateCloudFront(imagePaths: string[], cb: (err: Error|null) => void) {
+function invalidateCloudFront(cb: (err: Error|null) => void) {
     cloudfront.listDistributions((err: AWSError, data: any) => {
         // Handle error
         if (err) {
@@ -47,48 +49,77 @@ function invalidateCloudFront(imagePaths: string[], cb: (err: Error|null) => voi
         }).Id
 
         // Create invalidation
+        console.log("Cloudfront invalidating /*")
         cloudfront.createInvalidation({
             DistributionId: distributionID,
             InvalidationBatch: {
                 CallerReference: "resize-" + Date.now(),
                 Paths: {
-                    Items: imagePaths || ["/*"],
+                    Items: ["/*"],
                     Quantity: 1,
                 },
             },
-        }, (err: AWSError, data: any) => {
+        }, (err: AWSError) => {
             if (err) console.log(err, err.stack);
             cb(err);
         })
     })
 }
 
-export function handler(event: any, context: any): void {
-    console.log("event ", JSON.stringify(event));
-    async.mapLimit(event.Records, 4, (record: any, cb: (err: Error|null, result?: any) => void): void => {
+/*
+ * Sample event put
+{
+  "Records": [
+    {
+      "s3": {
+        "object": {
+          "key": "HappyFace.jpg",
+          ...
+        },
+        "bucket": {
+          "arn": bucketarn,
+          "name": "sourcebucket",
+          ...
+        },
+      },
+      ...
+      "eventName": "ObjectCreated:Put",
+      "eventSource": "aws:s3"
+    }
+  ]
+}
+*/
+function handlePutEvent(records: object[], cb: (err1?: Error|null) => void): void {
+    // retrieve all the matching images
+    async.mapLimit(records, 4, (record: any, cb: (err: Error|null, result?: any) => void): void => {
         const originalKey = decodeURIComponent(record.s3.object.key.replace(/\+/g, " "));
         s3.getObject({
             Bucket: record.s3.bucket.name,
             Key: originalKey,
         }, (err: AWSError, data: any) => {
             if (err) {
-                cb(err);
+                cb(null, {} /*err*/);
             } else {
-                cb(null, {
+                const imageType = getImageType(data.ContentType);
+                if (_.isEmpty(imageType)) cb(null, {})
+                else cb(null, {
                     buffer: data.Body,
                     contentType: data.ContentType,
-                    imageType: getImageType(data.ContentType),
+                    imageType,
                     originalKey,
                     record,
                 });
             }
         });
     }, (err?: Error|null, images?: string[]) => {
+        if (images) images = _.filter(images);
         if (err) {
-            context.fail(err);
+            cb(err);
+        } else if (!_.size(images)) {
+            cb();
         } else {
-            const imagePaths: string[] = []
-            const resizePairs = cross(["1200x750", "360x225"], images);
+            // produce image thumbnails
+            const resizePairs = cross(sizes, images);
             async.eachLimit(resizePairs, 4, (resizePair: any[], cb: (err: Error|null, result?: any) => void) => {
                 const config = resizePair[0]
                 const image = resizePair[1]
@@ -97,34 +128,92 @@ export function handler(event: any, context: any): void {
                 const height = config.split("x")[1]
                 let operation = im(image.buffer).resize(width, height, "^")
 
-                imagePaths.push(image.originalKey)
-
                 if (config === "360x225") {
                     operation = operation.gravity("Center").crop(width, height);
                 }
 
                 operation.toBuffer(image.imageType, (err3: Error|null, buffer: any) => {
-                    if (err3) {
-                        cb(err3);
-                    } else {
+                    if (err3) cb(err3);
+                    else {
+                        const resizePath = "pics/resized/" + config + "/" + relativePath
+                        console.log("putting resize image", resizePath)
                         s3.putObject({
                             Body: buffer,
                             Bucket: bucketName,
                             ContentType: image.contentType,
-                            Key: "pics/resized/" + config + "/" + relativePath,
-                        }, (err2: AWSError) => {
-                            cb(err2);
-                            imagePaths.push("pics/resized/" + config + "/" + relativePath)
-                        });
+                            Key: resizePath,
+                        }, cb);
                     }
                 });
-            }, (err1?: Error|null) => {
-                if (err1) context.fail(err1);
-                else invalidateCloudFront(imagePaths, (err2: Error|null) => {
-                    if (err2) context.fail(err2)
-                    else context.succeed();
-                })
-            });
+            }, cb);
+        }
+    });
+}
+
+/*
+ * Sample event delete
+{
+  "Records": [
+    {
+      ...
+      "s3": {
+        "object": {
+          "key": "HappyFace.jpg"
+          ...
+        },
+        "bucket": {
+          "arn": bucketarn,
+          "name": "sourcebucket"
+          ...
+        }
+      },
+      ...
+      "eventName": "ObjectRemoved:Delete",
+      "eventSource": "aws:s3"
+    }
+  ]
+}
+ */
+function handleDeleteEvent(records: object[], cb: (err1?: Error|null) => void): void {
+    // retrieve all the matching images
+    async.mapLimit(records, 4, (record: any, cb: (err: Error|null) => void): void => {
+        const originalKey = decodeURIComponent(record.s3.object.key.replace(/\+/g, " "));
+        const relativePath = originalKey.replace("pics/original/", "")
+        const resizePairs = cross(sizes, [originalKey]);
+
+        async.eachLimit(resizePairs, 4, (resizePair: any[], cb: (err: Error|null, result?: any) => void) => {
+            const config = resizePair[0]
+            const image = resizePair[1]
+            const resizePath = "pics/resized/" + config + "/" + relativePath
+
+            console.log("deleting resized image", resizePath)
+            s3.deleteObject({
+                Bucket: bucketName,
+                Key: resizePath,
+            }, cb);
+        })
+    }, cb);
+}
+
+export function handler(event: any, context: any): void {
+    console.log("event ", JSON.stringify(event));
+
+    const records = event.Records || [];
+    const putRecords = _.filter(records, r => _.isEqual(_.get(r, "eventName"), "ObjectCreated:Put"))
+    const deleteRecords = _.filter(records, r => _.isEqual(_.get(r, "eventName"), "ObjectRemoved:Delete"))
+    console.log("got", _.size(putRecords), "records on put");
+    console.log("got", _.size(deleteRecords), "records on delete");
+
+    handlePutEvent(putRecords, (err?: Error|null) => {
+        if (err) context.fail(err);
+        else {
+            handleDeleteEvent(deleteRecords, (err?: Error | null) => {
+                if (err) context.fail(err);
+                else invalidateCloudFront((err3: Error|null) => {
+                    if (err3) context.fail(err);
+                    else context.succeed()
+                });
+            })
         }
     });
 }
